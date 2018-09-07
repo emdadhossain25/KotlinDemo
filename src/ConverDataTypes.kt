c90396623025ce5e8520b5a75af4f12666443d9e
emdad <emdad.hossain321@gmail.com>
2018-09-08 01:29:53 +0600

    convert data type example

---
+++ src/ConverDataTypes.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+
+
+    // convert between data type
+
+    var intValue1:Int = 12
+    var floatValue1:Float = 1.2f
+    var mixedValue:Float = intValue1.toFloat()
+    println("floatValue1 : $floatValue1")
+    println(mixedValue)
+}
