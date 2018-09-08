b5adba010546003f56210cf36bf55b4e1ad58a05
emdad <emdad.hossain321@gmail.com>
2018-09-08 15:46:07 +0600

    added whenCOndition statment with range and other things

--- src/WhenConditionStatement.kt
+++ src/WhenConditionStatement.kt
@@ -1,25 +1,20 @@
 fun main(args: Array<String>) {
 
-    var x: Int = 11;
+    var x: Int ?;
+    x = 0
+    //more then one block of code
+    //more then one block of code
+    when(x!!){
 
-
-    when(x){
-
-        // checking the value of x and printing the values like switch in java
+    // checking the value of x and printing the values like switch in java
         1,2->
 
             println("Value of x is $x")
         in 5..10->
-        {
-            //more then one block of code
             println("Value of x is $x")
-        }
 
         !in 5..10->
-        {
-            //more then one block of code
             println("not Value of x is $x")
-        }
         else ->{
             println("value of x is $x")
         }
