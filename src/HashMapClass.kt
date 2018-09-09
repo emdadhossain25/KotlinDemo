d74099e396d1de37bd5cddf5cb764fa1c47c3ced
Emdad Hossain <emdad.hossain321@gmail.com>
2018-09-09 15:13:12 +0600

    added hashmap

---
+++ src/HashMapClass.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    // saves the value in key value pair
+    var map = HashMap<Int,String>()
+    map.put(0,"emdad");
+    map.put(1,"habib")
+    map.put(2,"faiyaz")
+
+    for (index in map.keys)
+    println("key: ${index} value : ${map.get(index)}")
+}
