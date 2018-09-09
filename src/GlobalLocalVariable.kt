b55ff2bfd320eeb06d9bed8c6a7611717704b72e
Emdad Hossain <emdad.hossain321@gmail.com>
2018-09-09 16:04:08 +0600

    added Global Variable text

---
+++ src/GlobalLocalVariable.kt
@@ -0,0 +1,12 @@
+
+var fullNmae = "Emdad Hossain"
+fun display():Unit{
+    println("name in display method: "+fullNmae)
+}
+fun main(args: Array<String>) {
+
+    var name = "hossain"
+    println("fullName: "+fullNmae)
+    println(name)
+    display()
+}
